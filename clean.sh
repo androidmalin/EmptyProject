02765840601b73e0fc93037bcfd3d55fe3f42314
androidmalin <malin.myemail@163.com>
2022-05-18 10:06:51 +0800

    upgrade dependencies

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -rf .idea/ .gradle/ 
 find . -name "build" -type d | xargs rm -rf
+find . -name ".gradle" -type d | xargs rm -rf
+find . -name ".idea" -type d | xargs rm -rf
 find . -name "*.iml" -type f | xargs rm -rf
