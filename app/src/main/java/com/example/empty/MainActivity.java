1cf94aaa53a2ec1d371eb6d722d2885e7dbdda42
androidmalin <malin.myemail@163.com>
2023-06-13 20:24:49 +0800

    update files

--- app/src/main/java/com/example/empty/MainActivity.java
+++ app/src/main/java/com/example/empty/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.empty;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.flutter.embedding.android.FlutterActivity;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        FlutterActivity
+                                .withNewEngine()
+                                .build(MainActivity.this)
+                );
+            }
+        });
     }
 }
---
+++ flutter_module/lib/page/recommend.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Recommend extends StatefulWidget {
+  const Recommend({Key? key}) : super(key: key);
+
+  @override
+  State<Recommend> createState() => _RecommendState();
+}
+
+class _RecommendState extends State<Recommend> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Recommend page');
+  }
+}
