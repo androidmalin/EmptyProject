8940f49d9c657a2acc30ee23d5384d7330005bee
androidmalin <malin.myemail@163.com>
2023-06-17 13:10:16 +0800

    add library config

--- app/src/main/java/com/example/empty/MainActivity.kt
+++ app/src/main/java/com/example/empty/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.empty
 
 import android.app.Activity
 import android.os.Bundle
+import com.example.library.LibViewModel
 
 class MainActivity : Activity() {
+
+    lateinit var viewModel: LibViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ library/src/main/java/com/example/library/LibraryActivity.java
@@ -0,0 +1,11 @@
+package com.example.library;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LibraryActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
